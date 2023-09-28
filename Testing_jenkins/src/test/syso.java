f03decc0fb3f6ffc9106d12bcbfbcb5a3342caba
Constanza <coe.elena47@gmail.com>
2023-09-28 19:45:46 -0300

    adding a java project

---
+++ Testing_jenkins/src/test/syso.java
@@ -0,0 +1,11 @@
+package test;
+
+public class syso {
+	public static void main(String[] args) {
+		System.out.println("Im a happy commit");
+		System.out.println("am I running?");
+		System.out.println("am I really really working?");
+		System.out.println("Fran, me estooy pisando?");
+		System.out.println("Fran, me estooy pisando DE NUEVO?");
+	}
+}
